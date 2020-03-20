74eee1769cf4eb90f3f99bd2ec54c6ed3b9ea1b8
lette1394 <lette1394@gmail.com>
2020-03-20 22:53:42 +0900

    Add storybook

---
+++ src/App.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export default { title: 'Button' };
+
+export const withText = () => <div>Hello div</div>;
+
+export const withEmoji = () => (
+  <div>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+    </div>
+);
