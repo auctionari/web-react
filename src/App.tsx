b683abc94a3fc7e2dd18cd03f4b1c1909c789496
lette1394 <lette1394@gmail.com>
2020-03-21 00:31:19 +0900

    Fix github actions

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
   return (
     <div className="App">
       <Button type="primary">Button</Button>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
