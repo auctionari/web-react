b1af53a03a131c01d1e27e43dac2b91e2ab00ee9
Jae Eun Na <lette1394@gmail.com>
2020-03-21 16:03:26 +0900

    Eject create-react-app (#16)
    - Fix test

--- src/App.stories.tsx
+++ src/App.stories.tsx
@@ -5,9 +5,9 @@ export default { title: 'Button' };
 export const withText = () => <div>Hello div</div>;
 
 export const withEmoji = () => (
-	<div>
-		<span role="img" aria-label="so cool">
-			😀 😎 👍 💯
-		</span>
-	</div>
+  <div>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </div>
 );
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Calendar } from 'antd';
 import './App.css';
 
+function onPanelChange(value: any, mode: any) {
+  console.log(value, mode);
+}
+
 function App() {
-	return (
-		<div className="App">
-			<Button type="primary">Button</Button>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <Calendar onPanelChange={onPanelChange} />
+    </div>
+  );
 }
 
 export default App;
